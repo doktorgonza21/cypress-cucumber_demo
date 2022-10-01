f69b85ec1e3e278cb9a3faf6fc3397876fabba8f
doktorgonza21 <gonza32167@gmail.com>
2022-10-02 00:22:25 +0300

    Final

--- cypress/e2e/step_definitions/betatest.js
+++ cypress/e2e/step_definitions/betatest.js
@@ -5,14 +5,14 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 Given('A user opens {string} page and scroll down to become a tester form', (url) => {
     cy.visit(url);
     beforePage.closeCookiesModalWindow()
-    cy.wait(2000)
+    cy.wait(3000)
     betatestPage.scrollToForm()
 })
 
 When('A user fills the form and clicks Submit button', () => {
     betatestPage.fillForm()
     betatestPage.clickApplyButton()
-    cy.wait(2000)
+    cy.wait(3000)
 })    
 
 Then('A user should see the {string} header message', (message) => {
